1554a86e51059d032f9aa500e5b3d96d6544d698
Joost Diepenmaat <joost@zeekat.nl>
2011-10-21 15:13:41 +0200

    remove dev-dependencies from project. makes the tests work again

--- project.clj
+++ project.clj
@@ -2,9 +2,4 @@
   :description "Clojure side of a refactoring tool"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [tcrayford_parsley/parsley "0.1.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [criterium "0.0.1-SNAPSHOT"]
-                     [lein-difftest "1.3.0"]
-                     [radagast "1.0.0"]]
-  :hooks [leiningen.hooks.difftest])
+                 [tcrayford_parsley/parsley "0.1.0"]])
