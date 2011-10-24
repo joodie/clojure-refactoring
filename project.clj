4253fb78d18982340e50e48050287b0aa2015dd0
Joost Diepenmaat <joost@zeekat.nl>
2011-10-24 21:24:45 +0200

    bump version, put license in README

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clojure-refactoring "0.5.0"
-  :description "Clojure side of a refactoring tool"
+(defproject clojure-refactoring "0.6.0"
+  :description "Clojure refactoring for Emacs/SLIME"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [net.cgrand/parsley "0.8.0"]])
