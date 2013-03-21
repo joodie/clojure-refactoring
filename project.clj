76756bd470d9b7e547950d0a1af3ef0baca24c86
Devin Walters <devinw@gmail.com>
2013-03-20 21:22:16 -0500

    Use midje for testing, rewrite tests
    - Moved test dir to old-test
    - Brought in midje dependency
    - Began process of converting old tests to pending tests under midje

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "Clojure refactoring for Emacs/SLIME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [net.cgrand/parsley "0.9.1"]
-                 [org.clojure/tools.namespace "0.2.2"]])
+                 [org.clojure/tools.namespace "0.2.2"]]
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]
+                   :plugins [[lein-midje "3.0.0"]]}})
