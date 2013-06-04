7ab69cccd5ccaa13d00422193aa57b68a4ec9e2a
luckykevin <htt5156@gmail.com>
2013-06-04 13:02:16 +0800

    Migrated local-binding and thread-expression tests.

---
+++ test/clojure_refactoring/t_thread_expression.clj
@@ -0,0 +1,38 @@
+(ns clojure-refactoring.t-thread-expression
+  (:use midje.sweet
+        clojure-refactoring.thread-expression
+        clojure.contrib.str-utils
+        [clojure-refactoring.support core formatter]))
+
+(fact "should return true if it is threaded"
+  (threaded? '(->> foo bar arr)) => truthy
+  (threaded? '(-> foo bar arr)) => truthy
+  (threaded? '(arr barr arr)) => falsey
+  (threaded? :atom) => falsey)
+
+;; Integration level tests below here
+(def start-src "(reduce + (map #(Integer. %) s))")
+(def end-src "(->> (map #(Integer. %) s)\n   (reduce +))")
+
+(fact "should turn expr into thread last form"
+  (thread-last start-src) => end-src)
+
+(fact "should turn expr into thread first form"
+  (thread-first "(+ (* c 1.8) 32)") => "(-> (* c 1.8)\n   (+ 32))")
+
+(fact "should turn thread last form back to origianl expr"
+  (thread-unthread "(->> sym (/ 1))") => "(/ 1 sym)\n"
+  (thread-unthread
+   "(->>
+    1
+    (rec-contains? (rest node))
+    (for [sym *binding-forms*])
+    (some #(= % true))
+    (not))") =>
+  "(not\n  (some\n    #(= % true)\n    (for [sym *binding-forms*] (rec-contains? (rest node) 1))))\n")
+
+(fact "should turn thread first form back to origianl expr"
+  (thread-unthread
+   "(-> 1
+ (/ 2))") => "(/ 1 2)\n"
+ (thread-unthread "(-> 1\n(/ 2) (+ 1))") => "(+ (/ 1 2) 1)\n")
