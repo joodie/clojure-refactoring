28b2ab8804a857930230a22d664e835abc9f5608
Ye He <htt5156@gmail.com>
2013-06-10 11:30:56 +0800

    Added deps specs.

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "Clojure refactoring for Emacs/nREPL"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [net.cgrand/parsley "0.9.1"]]
-  :profiles {:dev {:dependencies [[midje "1.5.0"]
-                                  [org.clojure/tools.trace "0.7.5"]
-                                  [org.clojure/tools.namespace "0.2.2"]]
+                 [net.cgrand/parsley "0.9.1"]
+                 [midje "1.5.0"]
+                 [org.clojure/tools.namespace "0.2.2"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.5"]]
                    :plugins [[lein-midje "3.0.0"]]}})
